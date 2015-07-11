b8fdf6f21e2866a51a9510374cf92c76d8a4f51d
shah <sjalal0228@gmail.com>
2015-07-10 23:05:48 -0500

    Mthods added for chrome and ie driver

--- src/main/java/com/pack/pageobject/TestBaseSetup.java
+++ src/main/java/com/pack/pageobject/TestBaseSetup.java
@@ -1,8 +1,11 @@
 package com.pack.pageobject;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
@@ -11,7 +14,10 @@ import org.testng.annotations.Test;
 
 public class TestBaseSetup {
 
-	private WebDriver driver;
+	protected WebDriver driver;
+	static String chromeDriverPath = "C:/Users/sjalal0228/Desktop/Selenium_Java/Drivers/chromedriver_win32/";
+	static String ieDriverPath = "C:/Users/sjalal0228/Desktop/Selenium_Java/Drivers/IEDriverServer_x64_2.46.0/";
+	
 	//private String baseUrl;
 	
 	
@@ -28,12 +34,12 @@ public class TestBaseSetup {
 			return driver;
 	}*/
 
-	public WebDriver getDriver(){
-		//System.out.println(driver);
+	public WebDriver getDriver() {
+		// System.out.println(driver);
 		return driver;
 	}
-	
-	private void setDriver(String browserType, String appUrl){
+
+	private void setDriver(String browserType, String appUrl) {
 		switch (browserType) {
 		case "chrome":
 			driver = initChromeDriver(appUrl);
@@ -50,46 +56,59 @@ public class TestBaseSetup {
 		}
 	}
 			
-	private static WebDriver initFirefoxDriver(String appURL){
+	private static WebDriver initFirefoxDriver(String appURL) {
 		System.out.println("Launching Firefox browser..");
 		WebDriver driver = new FirefoxDriver();
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.navigate().to(appURL);
-		//driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		return driver;
 	}
 	
-	private  WebDriver initChromeDriver(String appURL){
-		//initialize chrome driver
+	private WebDriver initChromeDriver(String appURL) {
+		// initialize chrome driver
+		System.out.println("Launching google chrome with new profile..");
+		System.setProperty("webdriver.chrome.driver", chromeDriverPath
+				+ "chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.navigate().to(appURL);
 		return driver;
 	}
 	
-	private WebDriver initIEDriver(String appURL){
-		//initialize Internet explorer
-		return driver;
+	private static WebDriver initIEDriver(String appURL){
+		//initialize IE driver
+		System.out.println("Launching Internet Explorer..");
+		System.setProperty("webdriver.ie.driver", ieDriverPath 
+				+"IEDriverServer.exe");
+		WebDriver driver = new InternetExplorerDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.navigate().to(appURL);
+		return driver;	
 	}
 	
 	
 	@BeforeClass
-	@Parameters ({"browserType", "appUrl"})
-	public void initializeTestBaseSetup(String browserType, String appUrl){
+	@Parameters({ "browserType", "appUrl" })
+	public void initializeTestBaseSetup(String browserType, String appUrl) {
 		try {
 			setDriver(browserType, appUrl);
-		} catch (Exception e){
+		} catch (Exception e) {
 			System.out.println("Error....." + e.getStackTrace());
 		}
 	}
-	
-	
+
 	@Test
-	@Parameters({"browserType", "appUrl"})
-	public void testing(String browser, String url ){
-		System.out.println("Browser Type: "+browser);
-		System.out.println("URL Address: "+url);
+	@Parameters({ "browserType", "appUrl" })
+	public void testing(String browser, String url) {
+		System.out.println("Browser Type: " + browser);
+		System.out.println("URL Address: " + url);
 	}
-	
+
 	@AfterClass
-	public void tearDown(){
+	public void tearDown() {
 		driver.quit();
 	}
 	
